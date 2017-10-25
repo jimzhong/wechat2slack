011f2e3165c6b15b3a4eac746904cf4e1952c595
Jim Zhong <hcgzhong@gmail.com>
2017-10-25 10:59:58 -0400

    new capture script for use with mitmdump

---
+++ capture.py
@@ -0,0 +1,13 @@
+from urllib.parse import urlsplit
+import requests
+
+SERVER_URL = "http://127.0.0.1:1234"
+
+def response(flow):
+
+    url = urlsplit(flow.request.url)
+    if url.netloc.endswith("wx2.qq.com"):
+        try:
+            requests.post(SERVER_URL + url.path, flow.response.content)
+        except:
+            print("POST error")
