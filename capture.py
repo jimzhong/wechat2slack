61301fc08e74e2df0b4beaf707f3155d11ecad68
Jim Zhong <hcgzhong@gmail.com>
2017-10-25 16:59:34 -0400

    test headers

--- capture.py
+++ capture.py
@@ -6,6 +6,8 @@ SERVER_URL = "http://127.0.0.1:1234"
 def response(flow):
 
     url = urlsplit(flow.request.url)
+    headers = flow.response.headers
+    pprint(headers)
     if url.netloc.endswith("wx2.qq.com"):
         try:
             requests.post(SERVER_URL + url.path, flow.response.content)
